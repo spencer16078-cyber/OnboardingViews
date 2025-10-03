b979966b253a5ab54de525d942904c01fa53d0a3
Spencer Searle <spencer16078@gmail.com>
2025-10-02 22:08:57 -0600

    Initial Commit

---
+++ OnboardingViews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnboardingViews
+//
+//  Created by Spencer Searle on 10/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnboardingViews/OnboardingViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewsApp.swift
+//  OnboardingViews
+//
+//  Created by Spencer Searle on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
